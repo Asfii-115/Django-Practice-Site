a78def4bdd8ab32088f6f0f6fc3de1635adb8923
Asfi <20101115@uap-bd.edu>
2024-07-10 00:51:36 +0600

    register view

--- postapp/post/views.py
+++ postapp/post/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, UserRegistrationForm
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+
 
 # Create your views here.
 def index(request):
@@ -50,5 +52,19 @@ def post_delete(request, post_id):
     post.delete()
     return redirect('post_list')
   return render(request, 'post_confirm_delete.html', {'post': post}) 
+
+def register(request):
+  if request.method=='POST':
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+     user = form.save(commit=False)
+     user.set_password(form.cleaned_data['password1'])
+     user.save()
+     login(request, user)
+     return redirect('post_list')
+    
+  else:
+    form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form}) 
 
 
