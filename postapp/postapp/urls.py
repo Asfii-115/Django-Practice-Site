6b09803371c4f295ee3c00bdb421281825ed7542
Asfi <20101115@uap-bd.edu>
2024-07-10 16:45:52 +0600

    fixes related to posting

--- postapp/postapp/urls.py
+++ postapp/postapp/urls.py
@@ -19,9 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.urls import views as auth_views
+from post import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('post/', include('post.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
